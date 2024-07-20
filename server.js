8ea2048ce396e1e63f5834be60f1f73283cbeec4
Shivaraj Umadi <shivaumadi@outlook.com>
2024-07-20 22:42:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = require('./redis');
 const { serialize } = require('cookie');
 
 const app = express();
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
