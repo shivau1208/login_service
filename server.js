58b1a42bcf943d79ae84b95ea2f41e9e71b85512
Shivaraj Umadi <shivaumadi@outlook.com>
2024-07-20 23:15:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Headers','Content-Type','Authorization')
+    next()
 })
 
 
